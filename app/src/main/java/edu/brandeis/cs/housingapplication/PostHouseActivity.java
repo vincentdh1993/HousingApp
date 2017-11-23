1ed9dcce21baec4083432dcd0135ca0d89d1ecd5
eureyuri <eureynoguchi@gmail.com>
2017-11-22 23:21:45 -0500

    Finished acticity except for saving to the database

--- app/src/main/java/edu/brandeis/cs/housingapplication/PostHouseActivity.java
+++ app/src/main/java/edu/brandeis/cs/housingapplication/PostHouseActivity.java
@@ -2,6 +2,9 @@ package edu.brandeis.cs.housingapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 /**
@@ -13,5 +16,24 @@ public class PostHouseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText addressText = (EditText)findViewById(R.id.address_text);
+                String address = addressText.getText().toString();
+
+                EditText descriptionText = (EditText)findViewById(R.id.description_text);
+                String description = descriptionText.getText().toString();
+
+                EditText priceText = (EditText)findViewById(R.id.price_text);
+                double price = Double.parseDouble(priceText.getText().toString());
+
+//                db.addHouse(address, description, price);
+                
+                finish();
+            }
+        });
     }
 }
